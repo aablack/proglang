2d652a44a3314427c1f29d8f68bdca056f696cd5
Adam Black <adam.black@iress.com.au>
2013-11-14 13:36:25 +1100

    finished hw5

--- programming_languages/week6/hw5.rkt
+++ programming_languages/week6/hw5.rkt
@@ -131,57 +131,133 @@
   (mlet "map" mupl-map
         (fun #f "i" (call (var "map") (fun #f "x" (add (var "i") (var "x")))))))
 
+
 ;; Challenge Problem
 
 (struct fun-challenge (nameopt formal body freevars) #:transparent) ;; a recursive(?) 1-argument function
 
-;; We will test this function directly, so it must do
-;; as described in the assignment
+; given a MUPL expression return a new MUPL expression
+; where fun is replaced by fun-challenge.
 (define (compute-free-vars e)
-  ; get the shadowed vars declared in a function but
-  ; do not go recursively into nested functions.
-  (define (get-shadowed-vars vars e)
-    (if (mlet? e)
-        (get-shadowed-vars (set-add vars (mlet-var e)) (mlet-body e))
-        vars))
-  
-  ; get all the variables referenced in an expression
-  (define (get-referenced-vars e)
-    (cond [(var? e) (set (var-string e))]
-          [(add? e) (set-union (get-referenced-vars (add-e1 e))
-                               (get-referenced-vars (add-e2 e)))]
-          [(ifgreater? e) (set-union (get-referenced-vars (ifgreater-e1 e))
-                                     (get-referenced-vars (ifgreater-e2 e))
-                                     (get-referenced-vars (ifgreater-e3 e))
-                                     (get-referenced-vars (ifgreater-e4 e)))]
-          [(call? e) (set (get-referenced-vars (call-actual e)))]
-          [(mlet? e) (set-union (get-referenced-vars (mlet-e e))
-                                (get-referenced-vars (mlet-body e)))]
-          [(apair? e) (set-union (get-referenced-vars (apair-e1 e))
-                                 (get-referenced-vars (apair-e2 e)))]
-          [(fun? e) (get-referenced-vars (fun-body e))]
+  ; given a function expression get free vars (gfv).
+  (define (gfv shadowed-vars e)
+    (cond [(var? e) (set-subtract (set (var-string e)) shadowed-vars)]
+          [(add? e) (set-union
+                     (gfv shadowed-vars (add-e1 e))
+                     (gfv shadowed-vars (add-e2 e)))]
+          [(ifgreater? e) (set-union
+                           (gfv shadowed-vars (ifgreater-e1 e))
+                           (gfv shadowed-vars (ifgreater-e2 e))
+                           (gfv shadowed-vars (ifgreater-e3 e))
+                           (gfv shadowed-vars (ifgreater-e4 e)))]
+          [(call? e) (set-union (gfv shadowed-vars (call-funexp e))
+                                (gfv shadowed-vars (call-actual e)))]
+          [(mlet? e) (set-union
+                      (gfv shadowed-vars (mlet-e e))
+                      (gfv (set-add shadowed-vars (mlet-var e)) (mlet-body e)))]
+          [(apair? e) (set-union
+                       (gfv shadowed-vars (apair-e1 e))
+                       (gfv shadowed-vars (apair-e2 e)))]
+          [(fun? e) (set-subtract (gfv (set (fun-formal e)) (fun-body e))
+                                  (set (fun-nameopt e)
+                                       (fun-formal e)))]
+          [(fst? e) (gfv shadowed-vars (fst-e e))]
+          [(snd? e) (gfv shadowed-vars (snd-e e))]
+          [(isaunit? e) (gfv shadowed-vars (isaunit-e e))]
           [#t (set)]))
      
-  (define (transform e)
     (cond [(fun? e)
            (fun-challenge (fun-nameopt e)
                           (fun-formal e)
-                          (transform (fun-body e))
-                          (set-subtract (get-referenced-vars (fun-body e))
-                                        (get-shadowed-vars (set) (fun-body e))
-                                        (set (fun-formal e))))]
-          [(apair? e) (apair (transform (apair-e1 e)) (transform (apair-e2 e)))]
+                          (compute-free-vars (fun-body e))
+                          (gfv (set) e))]
           [(mlet? e)
-           (mlet (mlet-var e)
-                 (mlet-e e)
-                 (transform (mlet-body e)))]
+           (mlet (mlet-var e) 
+                 (compute-free-vars (mlet-e e))
+                 (compute-free-vars (mlet-body e)))]
+          [(ifgreater? e)
+           (ifgreater (ifgreater-e1 e)
+                      (ifgreater-e2 e)
+                      (compute-free-vars (ifgreater-e3 e))
+                      (compute-free-vars (ifgreater-e4 e)))]
+          [(apair? e)
+           (apair (compute-free-vars (apair-e1 e)) (compute-free-vars (apair-e2 e)))]
+          [(add? e)
+           (add (compute-free-vars (add-e1 e)) (compute-free-vars (add-e2 e)))]
+          [(call? e)
+           (call (compute-free-vars (call-funexp e)) (compute-free-vars (call-actual e)))]
+          [(fst? e)
+           (fst (compute-free-vars (fst-e e)))]
+          [(snd? e)
+           (snd (compute-free-vars (snd-e e)))]
+          [(isaunit? e)
+           (isaunit (compute-free-vars (isaunit-e e)))]
           [#t e]))
-  (transform e))
   
 ;; Do NOT share code with eval-under-env because that will make
 ;; auto-grading and peer assessment more difficult, so
 ;; copy most of your interpreter here and make minor changes
-(define (eval-under-env-c e env) "CHANGE")
+(define (eval-under-env-c e env)
+    (define (get-pair e)
+    (let ([v (eval-under-env-c e env)])
+           (if (apair? v)
+               v
+               (error "MUPL expression is not a pair"))))
+    
+  (cond [(var? e)
+         (envlookup env (var-string e))]
+        [(int? e) e]
+        [(closure? e) e]
+        [(aunit? e) e]
+        [(add? e) 
+         (let ([v1 (eval-under-env-c (add-e1 e) env)]
+               [v2 (eval-under-env-c (add-e2 e) env)])
+           (if (and (int? v1)
+                    (int? v2))
+               (int (+ (int-num v1) 
+                       (int-num v2)))
+               (error "MUPL addition applied to non-number")))]
+        [(ifgreater? e)
+         (let ([v1 (eval-under-env-c (ifgreater-e1 e) env)]
+               [v2 (eval-under-env-c (ifgreater-e2 e) env)])
+           (if (and (int? v1)
+                    (int? v2))
+               (if (> (int-num v1) (int-num v2))
+                   (eval-under-env-c (ifgreater-e3 e) env)
+                   (eval-under-env-c (ifgreater-e4 e) env))
+               (error "MUPL ifgreater applied to a non-number")))]
+        [(mlet? e)
+         (let ([var (mlet-var e)]
+               [val (eval-under-env-c (mlet-e e) env)])
+           (eval-under-env-c (mlet-body e) (cons (cons var val)
+                                               env)))]
+        [(apair? e)
+         (apair (eval-under-env-c (apair-e1 e) env)
+                (eval-under-env-c (apair-e2 e) env))]
+        [(fst? e)
+         (apair-e1 (get-pair (fst-e e)))]
+        [(snd? e)
+         (apair-e2 (get-pair (snd-e e)))]
+        [(isaunit? e)
+         (let ([v (eval-under-env-c (isaunit-e e) env)])
+           (int (if (aunit? v) 1 0)))]
+        [(fun-challenge? e)
+         (closure (set-map (fun-challenge-freevars e) (lambda (var) (assoc var env)))
+                  e)]
+        [(call? e)
+         (let ([cl (eval-under-env-c (call-funexp e) env)]
+               [param (eval-under-env-c (call-actual e) env)])
+           (let ([cenv (closure-env cl)]
+                 [fun (closure-fun cl)])
+             (let ([fname (fun-challenge-nameopt fun)]
+                   [fargname (fun-challenge-formal fun)]
+                   [fbody (fun-challenge-body fun)])
+               (let ([extenv (cons (cons fargname param)
+                                   (if fname
+                                       (cons (cons fname cl) cenv)
+                                       cenv))])
+                 (eval-under-env-c fbody extenv)))))]
+        [#t (error "Unrecognised MUPL expression " e)]))
 
 ;; Do NOT change this
 (define (eval-exp-c e)
